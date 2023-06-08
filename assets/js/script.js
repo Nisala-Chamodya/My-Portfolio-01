03f4bd4765e764af57b1b7bcb6834fafe7b11512
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2023-06-09 01:13:00 +0530

    With UI Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,107 @@
 /*======================toggle icon navbar ===================*/
-let menuIcon=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
-
 
 /*======================Scroll section active link===================*/
-let sections=document.querySelectorAll('section');
-let navLinks=document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll =() => {
-    sections.forEach(sec =>{
-        let top =window.scrollY;
-        let offset=sec.offsetTop - 150;
-        let height =sec.offsetHeight;
-        let id=sec.getAttribute('id');
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*='+id +']').classList.add('active');
-            });
-        };
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  /*======================sticky navbar===================*/
+  let header = document.querySelector("header");
 
-    });
-    /*======================sticky navbar===================*/
-    let header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    header.classList.toggle('sticky',window.scrollY > 100);
-
-     /*======================remove toggle icon and navbar when  click navbar link (scroll)===================*/
-     menuIcon.classList.remove('bx-x');
-     navbar.classList.remove('active');
-
+  /*======================remove toggle icon and navbar when  click navbar link (scroll)===================*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
-  /*======================scroll reveal===================*/
-  ScrollReveal({ 
-    //reset: true,
-    distance : '80px',
-    duration: 2000,
-    delay:200
- });
+/*======================scroll reveal===================*/
+ScrollReveal({
+  //reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
 
- ScrollReveal().reveal('.home-content, .heading', { origin : 'top' });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 
- ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{ origin : 'bottom' });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
 
- ScrollReveal().reveal('.home-content h1, .about-img',{ origin : 'left' });
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
 
- ScrollReveal().reveal('.home-content p, .about-content',{ origin : 'right' });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
-  /*======================typed js===================*/
-  const typed=new Typed('.multiple-text',{
-    strings :['Fullstack Developer','Student','Photographer', 'UI / UX Designer'],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+/*======================typed js===================*/
 
-
-  });
-
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Fullstack Developer",
+    "Student",
+    "Photographer",
+    "UI / UX Designer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
 
 /*about me multiple text */
 
-const aboutme=new Typed('.about_memultiple-text',{
-    strings :['Fullstack Developer','Student','Photographer', 'UI / UX Designer'],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-    
-
-
-  });
-
-/*education typing */
-const educationTyping=new Typed('.educationtype',{
-  strings :['2006 -2010 Primary Education-Dhammananda Central Colleage','2010 -2018 Ordinary Level- Bandaragama Central Colleage','2019 -2020 Diploma In Infomation Technology-ESOFT', '2019 -2022 Advanced Level-Wevita Maithree College','2022 -Present Diploma In Infomation Technology-IJSE'],
-  typeSpeed:50,
-  backSpeed:20,
-  backDelay:1000,
-  loop:true
-  
-
-
+const aboutme = new Typed(".about_memultiple-text", {
+  strings: [
+    "Fullstack Developer",
+    "Student",
+    "Photographer",
+    "UI / UX Designer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
 
+/*education typing */
+const educationTyping = new Typed(".educationtype", {
+  strings: [
+    "2006 -2010 Primary Education-Dhammananda Central Colleage",
+    "2010 -2018 Ordinary Level- Bandaragama Central Colleage",
+    "2019 -2020 Diploma In Infomation Technology-ESOFT",
+    "2019 -2022 Advanced Level-Wevita Maithree College",
+    "2022 -Present Diploma In Infomation Technology-IJSE",
+  ],
+  typeSpeed: 50,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true,
+});
+/*typingSound();*/
 
-  
+function typingSound() {
+  $("audio")[0].play();
+}
